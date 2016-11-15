2f4390a0165d5198618c998f5e1e488cc49fed3e
DmitriyKizama <dmitrikkizama@gmail.com>
2016-11-15 20:28:20 +0200

    zaebalsya, nihuya ne poluchaetsya

--- app/src/main/java/com/stzemo/customgridview/adapters/SwipeAdapter.java
+++ app/src/main/java/com/stzemo/customgridview/adapters/SwipeAdapter.java
@@ -1,13 +1,12 @@
 package com.stzemo.customgridview.adapters;
 
 import android.content.Context;
-import android.graphics.Bitmap;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.stzemo.customgridview.R;
@@ -22,11 +21,18 @@ public class SwipeAdapter extends BaseAdapter {
     private Context context;
     private List<Person> list;
     private BottomControllerListener bListener;
+    private ViewHolder firstViewHolder;
 
     public SwipeAdapter(Context c, List<Person> list, BottomControllerListener bottomControllerListener) {
         this.list = list;
         context = c;
         bListener = bottomControllerListener;
+    }
+
+    private static class ViewHolder {
+        ImageView im;
+        View red;
+        View blue;
     }
 
     @Override
@@ -46,14 +52,24 @@ public class SwipeAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
+        ViewHolder viewHolder;
         if (view == null) {
             view = LayoutInflater.from(context).inflate(R.layout.swipe_item, viewGroup, false);
+            viewHolder = new ViewHolder();
+            viewHolder.im = (ImageView) view.findViewById(R.id.photo_swipe_adapter);
+            viewHolder.red = view.findViewById(R.id.im_red);
+            viewHolder.blue = view.findViewById(R.id.im_blue);
+            view.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) view.getTag();
         }
-        ImageView photo = (ImageView) view.findViewById(R.id.photo_swipe_adapter);
-        ImageLoader.getInstance().displayImage(list.get(i).urlPhoto, photo);
+        if (i == 0) {
+            firstViewHolder = viewHolder;
+        }
+        FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(ScreenParametrs.getWidth(), ScreenParametrs.getHeight() - 191);
+        view.setLayoutParams(layoutParams);
+        ImageLoader.getInstance().displayImage(list.get(i).urlPhoto, viewHolder.im);
 
-        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ScreenParametrs.getWidth(), ScreenParametrs.getWidth());
-        photo.setLayoutParams(layoutParams);
         return view;
     }
 
@@ -64,15 +80,34 @@ public class SwipeAdapter extends BaseAdapter {
     }
 
     public void remove() {
+        if (list.isEmpty()) {
+            return;
+        }
         bListener.onPhotoRemovedFromBottom(list.get(0));
         list.remove(0);
         notifyDataSetChanged();
     }
 
     public void next() {
+        if (list.isEmpty()) {
+            return;
+        }
         Person p = list.get(0);
         list.add(p);
         list.remove(0);
         notifyDataSetChanged();
+    }
+
+    public void scroll(float v) {
+        if (firstViewHolder == null) {
+            return;
+        }
+        if (v < 0) {
+            firstViewHolder.red.setAlpha(-v / 2);
+            firstViewHolder.blue.setAlpha(0);
+        } else {
+            firstViewHolder.blue.setAlpha(v / 2);
+            firstViewHolder.red.setAlpha(0);
+        }
     }
 }
